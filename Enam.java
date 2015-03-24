d2b4aeb4c35b975576a6f7cdf001768651ed49d0
bimayuliaputra <bimamojt@gmail.com>
2015-03-24 22:07:32 +0700

    Nambah satu pak, hehe

---
+++ Enam.java
@@ -0,0 +1,13 @@
+public class  Enam {
+	public static void main(String[] args) {
+	int total=0;
+	int awal=1;
+	int akhir=10;
+	for (int a=1;a<=10 ;a++ ) {
+	
+	total += awal;
+	awal++;
+	System.out.print(" = "+total);
+	}
+}
+}
