eeeb87fb3f3ea129644a7b1425d5f900de3489ad
bimayuliaputra <bimamojt@gmail.com>
2015-03-24 22:13:37 +0700

    Maaf pak
    Bagini benar apa salah pak? heheeee

---
+++ Tujuh.java
@@ -0,0 +1,13 @@
+public class  Tujuh {
+	public static void main(String[] args) {
+	float total=0;
+	int awal=1;
+	int akhir=10;
+	for (int a=1;a<=10 ;a++ ) {
+	
+	total += awal;
+	awal++;
+	System.out.print(" = "+total/10);
+	}
+}
+}
